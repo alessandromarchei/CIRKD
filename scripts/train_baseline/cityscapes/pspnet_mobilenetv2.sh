cf395f2868d46c53e1da712ae4a0f3b675b7ad6c
Alessandro Marchei <alessandromarchei@github.com>
2025-12-22 18:55:13 +0100

    added pspmovilenet v2 as a script

---
+++ scripts/train_baseline/cityscapes/pspnet_mobilenetv2.sh
@@ -0,0 +1,12 @@
+python train_baseline.py \
+    --model psp_mobile \
+    --backbone mobilenetv2 \
+    --dataset citys \
+    --batch-size 8 \
+    --lr 0.01 \
+    --max-iterations 80000 \
+    --data /home/sergey/DEV/AI/datasets/cityscapes/ \
+    --save-dir checkpoints/baseline/cityscapes/ \
+    --save-dir-name psp_mobile_mobilenetv2_citys_baseline \
+    --pretrained-base /home/sergey/DEV/AI/AEI/pretrained_backbones/mobilenet_v2.pth
+
