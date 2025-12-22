aabb54030094b78b91080cbe2ce7f2fc0ef96a7f
Alessandro Marchei <alessandromarchei@github.com>
2025-12-22 23:40:31 +0100

    added new scritps

---
+++ scripts/evaluation/cityscapes/deeplabv3_resnet101.sh
@@ -0,0 +1,12 @@
+python test.py \
+    --model deeplabv3 \
+    --backbone resnet101 \
+    --dataset citys \
+    --data /home/sergey/DEV/AI/datasets/cityscapes/ \
+    --save-dir runs/cityscapes/visualization \
+    --save-pred \
+    --pretrained deeplabv3_resnet101_citys_best_model.pth
+
+
+
+
